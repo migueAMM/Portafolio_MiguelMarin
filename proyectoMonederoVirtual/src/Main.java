7a17e9c661ffe80e05380d5be2f4660221d21f58
Miguel Angel Marin Montes <mamarinmontes@gmail.com>
2025-10-29 18:59:38 -0500

    First commit, just testing the project

---
+++ proyectoMonederoVirtual/src/Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+
+    for (int i = 1; i <= 5; i++) {
+        System.out.print(i + " ");
+    }
+}
